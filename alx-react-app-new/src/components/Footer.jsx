73fe5ca1b8828ac29de64c865d44d5d1c49bb099
Yasmin Khaled <yasmin.khaled.f@gmail.com>
2024-11-10 00:36:29 +0200

    adding task 1 styling css

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <p style={{ color: 'green' }}>© 2023 City Lovers</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
