be2bcc5811876362ae536d3eac552fa07b5071c0
Yasmin Khaled <yasmin.khaled.f@gmail.com>
2024-12-07 01:25:31 +0200

    adding task 2 files

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -2,47 +2,50 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function HomePage() {
-    const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState([]);
 
-    /**
-     *hook to implement side effects like data fetching
-     *its args are function, and dependant that is very important as it can control when the useEffect will be called,
-     *without it will be called on every render, so adding an empty array will make it be called on initialization only.
-     */
-    useEffect(() => {
-        fetch("/data.json")
-        .then((response) => response.json())
-        .then((data) => setRecipes(data));
-    }, []);
+  /**
+   *hook to implement side effects like data fetching
+   *its args are function, and dependant that is very important as it can control when the useEffect will be called,
+   *without it will be called on every render, so adding an empty array will make it be called on initialization only.
+   */
+  useEffect(() => {
+    fetch("/data.json")
+      .then((response) => response.json())
+      .then((data) => setRecipes(data));
+  }, []);
 
-    if (!recipes) {
-        return <div>Loading...</div>;
-      }
+  if (!recipes) {
+    return <div>Loading...</div>;
+  }
 
-    return(
-        <div className="container mx-auto p-4">
-            <h1 className="text-2xl font-bold mb-4 text-center">Recipes</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                {recipes.map((recipe) => (
-                    <div
-                        key={recipe.id}
-                        className="bg-white rounded-lg shadow-lg p-4 hover:shadow-xl transition-shadow duration-300"
-                        >
-                        <img
-                            src={recipe.image}
-                            alt={recipe.title}
-                            className="w-full h-40 object-cover rounded-t-lg"
-                        />
-                        <h2 className="text-xl font-semibold my-2">{recipe.title}</h2>
-                        <p className="text-gray-600">{recipe.summary}</p>
-                        <Link to={`/recipe/${recipe.id}`} className="text-blue-500 hover:text-blue-700">
-                            View Recipe
-                        </Link>
-                    </div>
-                ))}
-            </div>
-       </div>
-    );
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4 text-center">Recipes</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {recipes.map((recipe) => (
+          <div
+            key={recipe.id}
+            className="bg-white rounded-lg shadow-lg p-4 hover:shadow-xl transition-shadow duration-300"
+          >
+            <img
+              src={recipe.image}
+              alt={recipe.title}
+              className="w-full h-40 object-cover rounded-t-lg"
+            />
+            <h2 className="text-xl font-semibold my-2">{recipe.title}</h2>
+            <p className="text-gray-600">{recipe.summary}</p>
+            <Link
+              to={`/recipe/${recipe.id}`}
+              className="text-blue-500 hover:text-blue-700"
+            >
+              View Recipe
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
