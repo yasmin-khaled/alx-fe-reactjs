33af04aae0e45cb50ff7613a6d0d7a194158dd03
Yasmin Khaled <yasmin.khaled.f@gmail.com>
2024-12-07 00:50:11 +0200

    adding task 2 files

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -17,7 +17,7 @@ function HomePage() {
     return(
         <div className="container mx-auto p-4">
             <h1 className="text-2xl font-bold mb-4 text-center">Recipes</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {recipes.map((recipe) => (
                     <div
                         key={recipe.id}
