58c0ac1c2886456e5acd8c01b56da3499ef69630
Yasmin Khaled <yasmin.khaled.f@gmail.com>
2024-12-14 03:07:01 +0200

    adding task 4 filesdeploy

--- github-user-search/tailwind.config.js
+++ github-user-search/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // You can set it to 'media' or 'class' if needed
   theme: {
     extend: {},
